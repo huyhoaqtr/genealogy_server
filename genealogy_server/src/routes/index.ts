53bd7df0064d8da5825b41b18e78aa3b1fcee2bb
huyhoaqtr <huyhoaqtr@gmail.com>
2024-12-13 01:13:48 +0700

    refactor: add hello route

--- genealogy_server/src/routes/index.ts
+++ genealogy_server/src/routes/index.ts
@@ -3,25 +3,30 @@
  * Date: 2024-10-24
  */
 
-import { Router } from 'express';
-import authRoutes from './auth.routes';
-import tribeRoutes from './tribe.routes';
-import messageRoutes from './message.routes';
-import eventRoutes from './event.routes';
-import fundRoutes from './fund.routes';
-import voteRoutes from './vote.routes';
-import feedRoutes from './feed.routes';
-import web3Routes from './web3.routes';
+import { Router } from "express";
+import authRoutes from "./auth.routes";
+import tribeRoutes from "./tribe.routes";
+import messageRoutes from "./message.routes";
+import eventRoutes from "./event.routes";
+import fundRoutes from "./fund.routes";
+import voteRoutes from "./vote.routes";
+import feedRoutes from "./feed.routes";
+import web3Routes from "./web3.routes";
 
 const router = Router();
 
-router.use('/auth', authRoutes);
-router.use('/tribe', tribeRoutes);
-router.use('/message', messageRoutes);
-router.use('/event', eventRoutes);
-router.use('/fund', fundRoutes);
-router.use('/vote', voteRoutes);
-router.use('/feed', feedRoutes);
-router.use('/web3', web3Routes);
+// hello world
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+router.use("/auth", authRoutes);
+router.use("/tribe", tribeRoutes);
+router.use("/message", messageRoutes);
+router.use("/event", eventRoutes);
+router.use("/fund", fundRoutes);
+router.use("/vote", voteRoutes);
+router.use("/feed", feedRoutes);
+router.use("/web3", web3Routes);
 
 export default router;
